677d99e94dbbcfcd0850e84c282558b60857cddf
MelaJia <838672863@qq.com>
2018-06-04 19:54:49 +0800

    ok

--- app/src/main/java/cn/edu/gdmec/android/newapplication/Movie/FgMovieFragment.java
+++ app/src/main/java/cn/edu/gdmec/android/newapplication/Movie/FgMovieFragment.java
@@ -9,19 +9,16 @@ import android.view.ViewGroup;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.util.TimerTask;
-
 import cn.edu.gdmec.android.newapplication.Bean.MovieBean;
 import cn.edu.gdmec.android.newapplication.Movie.Presenter.MoviePresenter;
 import cn.edu.gdmec.android.newapplication.Movie.View.IMovieView;
-import cn.edu.gdmec.android.newapplication.News.Presenter.NewPresenter;
 import cn.edu.gdmec.android.newapplication.R;
 
 
 public class FgMovieFragment extends Fragment implements IMovieView {
 
     private MoviePresenter presenter;
-    private SwipeRefreshLayout srl_news;
+    private SwipeRefreshLayout srl_moves;
     private String total;
     private TextView tv_movie;
 
@@ -35,13 +32,13 @@ public class FgMovieFragment extends Fragment implements IMovieView {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         tv_movie=view.findViewById(R.id.tv_movie);
-        srl_news=view.findViewById(R.id.srl_news);
-        srl_news.setColorSchemeColors(Color.parseColor("#ffce3d3a"));
+        srl_moves =view.findViewById(R.id.srl_news);
+        srl_moves.setColorSchemeColors(Color.parseColor("#ffce3d3a"));
         presenter=new MoviePresenter((IMovieView) this);
-        srl_news.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        srl_moves.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                presenter.loadMovie("");
+                presenter.loadMovie("in_theaters");
             }
         });
 
@@ -50,27 +47,24 @@ public class FgMovieFragment extends Fragment implements IMovieView {
 
 
     @Override
-    public void showMovie(final MovieBean movieBean) {
-        getActivity().runOnUiThread(new TimerTask() {
-            @Override
-            public void run() {
-                tv_movie.setText(movieBean.getTotal());
-            }
-        });
+    public void showMovie(MovieBean movieBean) {
+        tv_movie.setText(movieBean.getSubjects().get(0).getTitle()+
+                ""+movieBean.getSubjects().get(0).getDirectors());
     }
 
     @Override
     public void hideDialog() {
-
+        srl_moves.setRefreshing(false);
     }
 
     @Override
     public void showDialog() {
-
+        srl_moves.setRefreshing(true);
     }
 
     @Override
     public void showErrorMsg(String error) {
+        tv_movie.setText("加载错误！");
 
     }
 }
